7cfbdd08a8650db78624b9312bb1ba66b9cfd22f
g78101@gmail.com <g78101@gmail.com>
2014-08-03 01:51:25 +0800

    Initial Commit

---
+++ Focal_Effects_for_Volume_Rendering/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Focal_Effects_for_Volume_Rendering
+//
+//  Created by Talka_Ying on 8/3/14.
+//  Copyright (c) 2014 Talka_Ying. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
